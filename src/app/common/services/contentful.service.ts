e4ab0d148daa7a5dbcfbfc6171b978cb0125f82a
Maxi <maxi.barmetler@gmail.com>
2021-03-17 12:43:05 +0100

    contentful cv search for Maxi Barmetler

--- src/app/common/services/contentful.service.ts
+++ src/app/common/services/contentful.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core'
-import { createClient } from 'contentful'
+import { createClient, CreateClientParams } from 'contentful'
 import { from, Observable } from 'rxjs'
 import { CV } from '../models/cv'
 import cvMock from '../mock/cv.json'
 import schemaJson from '../schemas/cv.schema.json'
 import { isValid, makeValidator } from '../models/validation'
 
-const CONFIG = {
+const CONFIG: CreateClientParams = {
     space: 'bp893k004d65',
     accessToken: 'P0yhCtlkmn78L5wM3dSY39iE5JcKAfBxMtAwHKl_0ow',
 }
@@ -18,16 +18,14 @@ const cvValidator = makeValidator<CV>(schemaJson)
 })
 export class ContentfulService {
 
-    protected client = createClient({
-        space: CONFIG.space,
-        accessToken: CONFIG.accessToken,
-    })
+    protected client = createClient(CONFIG)
 
     constructor() { }
 
     getCV(): Observable<CV> {
         return from(this.client.getEntries({
-            content_type: 'cv'
+            content_type: 'cv',
+            'fields.name[match]': 'Maxi Barmetler'
         }).catch(() => null).then(res => {
             const result = res?.items?.[0]?.fields
             console.log(result)
